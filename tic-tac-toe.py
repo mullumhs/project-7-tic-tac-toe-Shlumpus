7938063efe026341df23e97964e7f3df1c5f70c7
Shlumpus <140124184+Shlumpus@users.noreply.github.com>
2024-04-04 23:38:34 +0000

    connect 4 progress

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,12 +1,16 @@
 board = []
 
 def initialiseBoard(): 
-
+    print("---CONNECT FOUR---")
+    print()
+    print("1 2 3 4 5 6 7 8 9")
     for i in range(7):
-        row = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+        digit = '-'
+        row = [digit, digit, digit, digit, digit, digit, digit, digit, digit, ]
         board.append(row)
 
 def displayBoard():
+
     for i in board:
         for digit in i:
             print(digit, end=' ')
@@ -16,19 +20,39 @@ initialiseBoard()
 displayBoard()
 player = 1
 
+def checkHorizontal(board):
+    for row in range:
+        for col in range(4):
+            if (board[row][col] == board[row][col + 1] == board[row][col + 2] == board[row][col + 3] != digit):
+                return True
+    return False
+
+def checkDiagonal(board):
+    for row in range(3):
+        for col in range(4):
+            
 
 while True:
-    
+    digit = '-'
+
     token = 'X'
     if player % 2 == 0:
         token = 'O'
-    player += 1
 
-    choice = int(input("Choose your column: "))
+    if player % 2 == 0:
+        choice = (input("Choose your column P2: "))
+    elif player % 2 != 0:
+        choice = (input("Choose your column P1: "))
+        
+    choice = int(choice)
     choice -= 1
 
     for i in range(6, -1, -1):
-        board[6][choice]
+        if board[i][choice] == digit:
+            board[i][choice] = token
+            player += 1
+            break
+
     
     displayBoard()
 
