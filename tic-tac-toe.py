fd7203c4e921a397e8f342bd9529862b58b4a4da
Shlumpus <140124184+Shlumpus@users.noreply.github.com>
2024-04-10 00:40:27 +0000

    fixing checks

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,14 +3,13 @@ board = []
 def initialiseBoard(): 
     print("---CONNECT FOUR---")
     print()
-    print("1 2 3 4 5 6 7 8 9")
     for i in range(7):
         digit = '-'
         row = [digit, digit, digit, digit, digit, digit, digit, digit, digit, ]
         board.append(row)
 
 def displayBoard():
-
+    print("1 2 3 4 5 6 7 8 9")
     for i in board:
         for digit in i:
             print(digit, end=' ')
@@ -21,8 +20,8 @@ displayBoard()
 player = 1
 
 def checkHorizontal(board):
-    for row in range(6):
-        for col in range(8):
+    for row in range(7):
+        for col in range(6):
             if (board[row][col] == board[row][col + 1] == board[row][col + 2] == board[row][col + 3] and board[row][col] == 'X'):
                 print("Player 1 WINS")
                 return True
@@ -32,7 +31,7 @@ def checkHorizontal(board):
     return False
 
 def checkVertical(board):
-    for col in range(8):
+    for col in range(9):
         for row in range(4):
             if (board[row][col] == board[row + 1][col] == board[row + 2][col] == board[row + 3][col] and board[row][col] == 'X'):
                 print("Player 1 WINS")
@@ -40,6 +39,18 @@ def checkVertical(board):
             if (board[row][col] == board[row + 1][col] == board[row + 2][col] == board[row + 3][col] and board[row][col] == 'O'):
                 print("Player 2 WINS")
                 return True
+    return False
+
+def checkDiagonal(board):
+    for row in range(4):
+        for col in range(6):
+            if (board[row][col] == board[row + 1][col + 1] == board[row + 2][col + 2] == board[row + 3][col + 3] != digit):
+                if token == 'X':
+                    print("Player 1 WINS")
+                    return True
+                elif token == 'O':
+                    print("Player 2 WINS")
+                    return True
     return False
 
 while True:
@@ -57,10 +68,7 @@ while True:
     choice = int(choice)
     choice -= 1
 
-    board[3][0] == 'X'
-    board[3][1] == 'X'
-    board[3][2] == 'X'
-    board[3][3] == 'X'
+    
 
     for i in range(6, -1, -1):
         if board[i][choice] == digit:
@@ -70,6 +78,8 @@ while True:
 
     
     displayBoard()
+    checkHorizontal(board)
+    checkVertical(board)
 
 
 
